d1190dd3762a98a41f5bed2d59c2ffae86b26768
caionsantos <137624365+caionsantos@users.noreply.github.com>
2025-06-03 21:53:41 -0300

    update com spf

---
+++ code/Matematica/fatoracao_prima_e_spf.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //SHORTEST PRIME FACTOR
+    vector<ll> spf(maxn+1, 0);
+    ll curr;
+    for(ll i = 2; i <= maxn; i++){
+        if(spf[i] == 0){
+            spf[i] = i;
+            curr = i*i;
+            while(curr <= maxn){
+                if(spf[curr] == 0){
+                    spf[curr] = i;
+                }
+                curr += i;
+            }
+        }
+    }
+
+    //FATORACAO MELHORADA COM SPF -> nlogn
+    ll v;
+    vector<map<ll, int>> fat(maxn+1);
+    for(int i = 2; i <= maxn; i++){
+        v = i;
+        while(v > 1){
+            porfavor[i][spf[v]]++;
+            v = v/spf[v];
+        }
+    }
+    
+    //FATORACAO PRIMA PADRAO -> n * sqrt(n)
+    int n;
+    vector<int> primos;
+    map<int, int> freq;
+    for(int i = 2; i*i <= n; i++){
+        int cnt = 0;
+        while(n % i == 0){
+            n /= i;
+            cnt++;
+        }
+        if(cnt > 0){
+            freq[i] += cnt;
+            primos.push_back(i);
+        }
+    }
+    if(n > 1){
+        primos.push_back(n);
+        freq[n]++;
+    }
+
+    return 0;
+}
