b4f1e7739c34f169698f2d03ce55684f097180bf
caionsantos <137624365+caionsantos@users.noreply.github.com>
2025-06-03 21:54:13 -0300

    compilavel?

--- code/Matematica/fatoracao_prima_e_spf.cpp
+++ code/Matematica/fatoracao_prima_e_spf.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define ll long long int
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    ll maxn;
     //SHORTEST PRIME FACTOR
     vector<ll> spf(maxn+1, 0);
     ll curr;
