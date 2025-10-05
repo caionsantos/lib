959e864e9b94b16452c33e5b69e905d987e9f141
caionsantos <137624365+caionsantos@users.noreply.github.com>
2025-10-05 16:53:15 -0300

    segree refact

--- code/Range Queries/segment_tree_base.cpp
+++ code/Range Queries/segment_tree_base.cpp
@@ -1,66 +1,68 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define ll long long
 using namespace std;
 
-void build(int no, int l, int r, vector<long long int> &segtree, vector<long long int> &orig){
-    if(l == r){
-        segtree[no] = orig[l];
-        return;
-    }
-
-    int mid = (l + r)/2;
-    build(2 * no, l, mid, segtree, orig);
-    build((2 * no) + 1, mid + 1, r, segtree, orig);
-    segtree[no] = segtree[2 * no] + segtree[(2 * no) + 1];
-}
-
-void update(int no, int l, int r, int pos, long long int val, vector<long long int> &segtree){
-    if((pos < l) or (r < pos)){
-        return;
-    }
-    if(l == r){
-        segtree[no] = val;
-        return;
-    }
-
-    int mid = (l + r)/2;
-    update(2* no, l, mid, pos, val, segtree);
-    update((2* no) + 1, mid + 1, r, pos, val, segtree);
-    segtree[no] = segtree[2 * no] + segtree[(2 * no)+ 1];
-
-}
-
-long long int query(int no, int l, int r, int lq, int rq, vector<long long int> &segtree){
-    if((rq < l) or (r < lq)){
-        return 0;
-    }
-    if((lq <= l) and (r <= rq)){
-        return segtree[no];
-    }
-
-    int mid = (l + r)/2;
-    long long int ans = query(2 * no, l, mid, lq, rq, segtree);
-    ans += query((2 * no) + 1, mid + 1, r, lq, rq, segtree);
-
-    return ans;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+struct Segtree{
+    vector<ll> segtree;
     int n;
-    vector<long long int> segtree(4 * n);
-    //ADICIONAR X EM L E -X EM R+1 EH IGUAL A ADICIONAR X EM [L, R]
-    //SEGTREE PARA ACHAR MENORES:
-    // for(int i = 0; i < n; i++){
-    //     range = upper_bound(mansort.begin(), mansort.end(), man[i]) - mansort.begin();
-    //     dir[i] = query(1, 1, n, range, n, segtree);
-    //     update(1, 1, n, range, 1, segtree);
-    // }
+    Segtree(vector<ll> &orig){
+        segtree.resize(4* (orig.size()));
+        n = orig.size();
+        build(1, 0, n-1, orig);
+    }
+    Segtree(int n){
+        segtree.resize(4*n);
+        this->n = n;
+    }
 
-    //SegEuler -> usar updates ao inves do build (ou mapear vetor a[euler_in[i]] - > orig[i]
-    // query em euler_in[i], euler_out[i]
-    // update em euler_in[i]
+    ll combine(ll left, ll right){
+        return left + right;
+    }
 
-    return 0;
-}
+    //build(0, 0, n-1)
+    void build(int no, int l, int r, vector<ll> &orig){
+        if(l == r){
+            segtree[no] = orig[l];
+            return;
+        }
+
+        int mid = (l + r)/2;
+        build(2 * no, l, mid, orig);
+        build((2 * no) + 1, mid + 1, r, orig);
+        segtree[no] = combine(segtree[2 * no], segtree[(2 * no) + 1]);
+    }
+
+    //update(1, 1, n)
+    void update(int no, int l, int r, int pos, ll val){
+        if((pos < l) or (r < pos)){
+            return;
+        }
+        if(l == r){
+            segtree[no] = val;
+            return;
+        }
+
+        int mid = (l + r)/2;
+        update(2* no, l, mid, pos, val);
+        update((2* no) + 1, mid + 1, r, pos, val);
+        segtree[no] = combine(segtree[2 * no], segtree[(2 * no) + 1]);
+
+    }
+
+    //query(1, 1, n)
+    ll query(int no, int l, int r, int lq, int rq){
+        if((rq < l) or (r < lq)){
+            return 0;
+        }
+        if((lq <= l) and (r <= rq)){
+            return segtree[no];
+        }
+
+        int mid = (l + r)/2;
+        ll ans = combine(query(2 * no, l, mid, lq, rq), query((2 * no) + 1, mid + 1, r, lq, rq));
+
+        return ans;
+    }
+
+};
