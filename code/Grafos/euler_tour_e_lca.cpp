76dc195fb589ab47e9b30a07c01a02e62074eac7
caionsantos <137624365+caionsantos@users.noreply.github.com>
2026-04-16 07:57:03 -0300

    refactor lca

--- code/Grafos/euler_tour_e_lca.cpp
+++ code/Grafos/euler_tour_e_lca.cpp
@@ -1,66 +1,80 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define ll long long
 using namespace std;
 
-int tempo = 0;
+const int LOG = 20;
+const int maxn = 2e5 + 10;
 
-void euler(int v, vector<vector<long long int>> &adj, vector<bool> &vis, vector<long long int> &euler_in, vector<long long int> &euler_out){
-    vis[v] = true;
-    tempo++;
-    euler_in[v] = tempo;
-    //euler tour
-    for(auto u : adj[v]){
-        if(!vis[u]){
-            euler(u, adj, vis, euler_in, euler_out);
+struct LCA{
+    int up[maxn][LOG+1];
+    int n;
+    int tempo = -1; //trocar pra 0 pra virar 1-indexado
+    vector<ll> in, out;
+    vector<vector<int>> adj;
+    vector<ll> dst;
+    LCA(int n){
+        memset(up, -1, sizeof up);
+        this->n = n;
+        adj.resize(n+1), in.resize(n+1), out.resize(n+1), dst.resize(n+1);
+    }
+
+    void add(int u, int v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void tour(int v, int p, ll d){
+        tempo++;
+        in[v] = tempo;
+
+        dst[v] = d;
+        up[v][0] = p;   
+        
+        for(auto u : adj[v]){
+            if(u!=p)tour(u, v, d+1);
+        }
+        out[v] = tempo;
+    }
+
+    void build(){
+        for(int j = 1; j <= LOG; j++){
+            for(int i = 1; i <= n; i++){
+                if(up[i][j-1] != -1){
+                    up[i][j] = up[up[i][j-1]][j-1];
+                }
+            }
         }
     }
-    //tempo++; -> desnecessario
-    euler_out[v] = tempo;
-}
 
-int lca(int a, int b, vector<long long int> &euler_in, vector<long long int> &euler_out, vector<vector<long long int>> &pai){
-    if((euler_in[a] <= euler_in[b]) and (euler_out[a] >= euler_out[b])){
-        return a;
-    } else if((euler_in[b] <= euler_in[a]) and (euler_out[b] >= euler_out[a])){
-        return b;
-    } else{
+    void init(){
+        tour(1, -1, 0);
+        build();
+    }
+
+    //a é ancestral de b?
+    inline bool is_ancestor(int a, int b){
+        return ((in[a] <= in[b]) && (out[a] >= out[b]));
+    }
+
+    int lca(int a, int b){
+        if(is_ancestor(a, b)) return a;
+        if(is_ancestor(b, a)) return b;
+
         int cursor = a;
-        for(int i = 20; i >= 0; i--){
-            if(!((euler_in[pai[cursor][i]] <= euler_in[b]) and (euler_out[pai[cursor][i]] >= euler_out[b]))){
-                cursor = pai[cursor][i];
+        for(int i = LOG; i >= 0; i--){
+            if(up[cursor][i] == -1) continue;
+            if(!(is_ancestor(up[cursor][i], b))){
+                cursor = up[cursor][i];
             }
         }
-        return pai[cursor][0];
-    }
-}
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, q, p, a, b;
-    //AJUSTAR VALOR DE ITERAÇÃO DE ACORDO COM LOGN
-    cin >> n >> q;
-    vector<vector<long long int>> pai(n+1, vector<long long int>(21, -1));
-    vector<long long int> euler_in(n+1);
-    vector<long long int> euler_out(n+1);
-    vector<vector<long long int>> adj(n+1);
-    vector<bool> vis(n+1, false);
-    pai[1][0] = 1;
-    for(int i = 2; i <= n; i++){
-        cin >> p;
-        pai[i][0] = p;
-        adj[p].push_back(i);
-        adj[i].push_back(p);
-    }
-    //precalcular pais
-    for(int j = 1; j <= 20; j++){
-        for(int i = 1; i <= n; i++){
-            if(pai[i][j - 1] != -1){
-                pai[i][j] = pai[pai[i][j-1]][j-1];
-            }
-        }
+        return up[cursor][0];
     }
 
-    return 0;
-}
+    ll dist(int a, int b){
+        int lc = lca(a, b);
+
+        return (dst[a] + dst[b] - (2 * dst[lc]));
+    }
+};
