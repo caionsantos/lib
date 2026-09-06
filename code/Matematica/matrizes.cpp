5b9a80724b757b28f6a06e1800940ebd230a0b54
caionsantos <137624365+caionsantos@users.noreply.github.com>
2026-09-06 11:18:44 -0300

    update matriz

--- code/Matematica/matrizes.cpp
+++ code/Matematica/matrizes.cpp
@@ -1,48 +1,56 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define ll long long
 using namespace std;
 
-vector<vector<long long int>> mult(vector<vector<long long int>> &a, vector<vector<long long int>> &b, long long int MOD){
-    vector<vector<long long int>> res(a.size(), vector<long long int>(b[0].size()));
-
+vector<vector<ll>> mult(vector<vector<ll>> &a, vector<vector<ll>> &b, ll MOD){
+    vector<vector<ll>> res(a.size(), vector<ll>(b[0].size()));
+ 
     for(int i = 0; i < a.size(); i++){
         for(int j = 0; j < b[0].size(); j++){
-            res[i][j] = 0;
             for(int k = 0; k < a[0].size(); k++){
                 res[i][j] += (a[i][k] * b[k][j]) % MOD;
                 res[i][j] = res[i][j] % MOD;
             }
         }
     }
+ 
+    return res;
+}
+
+//muito mais rapido
+vector<vector<ll>> mult2(vector<vector<ll>> &a, vector<vector<ll>> &b, ll MOD){
+    vector<vector<ll>> res(a.size(), vector<ll>(b[0].size()));
+
+    for(int i = 0; i < a.size(); i++){
+        vector<unsigned __int128> temp(b[0].size(), 0); 
+        
+        for(int k = 0; k < a[0].size(); k++){
+            if(a[i][k] == 0) continue;
+            for(int j = 0; j < b[0].size(); j++){
+                temp[j] += (unsigned __int128)a[i][k] * b[k][j];
+            }
+        }
+        
+        for(int j = 0; j < b[0].size(); j++){
+            res[i][j] = (ll)(temp[j] % MOD);
+        }
+    }
 
     return res;
 }
 
-vector<vector<long long int>> fexp(vector<vector<long long int>> &a, long long int e, long long int MOD){
-    vector<vector<long long int>> ans(4, vector<long long int>(4, 0));
-    ans[0][0] = 1;
-    ans[1][1] = 1;
-    ans[2][2] = 1;
-    ans[3][3] = 1;
+vector<vector<ll>> fexp(vector<vector<ll>> &a, ll e, ll MOD){
+    vector<vector<ll>> ans(a.size(), vector<ll>(a.size(), 0));
+    for(int i = 0; i < a.size(); i++)ans[i][i] = 1;
 
     while(e){
         if(e & 1){
-            ans = mult(a, ans, MOD);
+            ans = mult(ans, a, MOD);
         }
         a = mult(a, a, MOD);
         e >>= 1;
     }
 
     return ans;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    //recorrencia em matrizes
-    //matrix T * matrix (f0) = matrix (f2)
-    //                  (f1)          (f1)
-
-    return 0;
 }
